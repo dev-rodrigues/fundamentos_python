77e0ebf37dfec9424e0442a82365e44b66241adf
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2020-08-15 16:55:58 -0300

    estrutura de repeticao

---
+++ estruturaderepeticao.py
@@ -0,0 +1,56 @@
+#Faça um programa que peça uma nota, entre zero e dez.
+# Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário
+# informe um valor válido.
+
+# nota_valida = False
+# nota_informada = 0
+
+# while (nota_valida == False):
+#     nota_informada = int(input('Informe uma nota'))
+#     if (nota_informada > 0 and nota_informada < 10):
+#         nota_valida = True
+
+# print('Nota informada: ', nota_informada)
+
+# usuario = ''
+# senha = ''
+# senha_valida = False
+
+# while (senha_valida = False):
+#     usuario = input('informe o usuario')
+#     senha = input('informe a senha')
+    
+#    if (usuario != senha):
+#       senha_valida = True
+
+#print('Usuario:', usuario)
+#print('Senha:', senha)
+
+#for numero in range(1, 21):
+#    print(numero)
+
+#numeros = []
+
+#for numero in range(1, 6):
+#    numero_informado = int(input('Informe um número'))
+#    numeros.append(numero_informado)
+
+#maior_numero = 0
+
+#for numero in numeros:
+#    if numero > maior_numero:
+#        maior_numero = numero
+
+# print('O maior número é: ', maior_numero)
+    
+# for numero in range (1, 51):
+#    if numero % 2 > 0:
+#        print(numero)
+
+# inicio = int(input('Informe o inicio'))
+# fim = int(input('Informe o fim'))
+
+# for numero in range(inicio, fim + 1):
+#    print(numero)
+
+
