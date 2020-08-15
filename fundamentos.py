7af356bf15528b850bcacf8a72c91375364ba80a
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2020-08-15 16:31:49 -0300

    fundamentos

---
+++ fundamentos.py
@@ -0,0 +1,57 @@
+# print('Olá mundo')
+
+# mensagem = input('Digite alguma coisa')
+#print('A mensagem digita foi: ', mensagem)
+
+# numero1 = int(input('Digite o número 1'))
+# numero2 = int(input('Digite o número 2'))
+
+# soma = int(numero1) + int(numero2
+# print(soma)
+
+# notas = []
+
+# for nota in range(1, 5):
+#    nota_informada = int(input('Informe a nota: '))
+#    notas.append(nota_informada)
+
+# somatorio = 0
+
+# for nota in notas:
+#    somatorio = int(nota) + somatorio
+
+# total_de_notas = len(notas)
+# media = somatorio / total_de_notas
+
+# print(media)
+
+
+# numeros = []
+
+# for numero in range(1, 5):
+#    valor_digitado = int(input('Digite o número 1'))
+#    numeros.append(valor_digitado)
+
+# maior_valor = 0
+
+# for valor_digitado in numeros:
+#    if valor_digitado > maior_valor:
+#        maior_valor = valor_digitado
+    
+# print('O maior valor é', maior_valor)
+
+# nota = float(input('Informe a nota'))
+
+# if nota == 0:
+#    print('Que Vergonha!')
+# elif nota > 0 and nota < 5:
+#    print('tem que estudar mais...')
+# elif nota >= 5 and nota < 8:
+#    print('Falta pouco')
+# elif nota <= 10:
+#    print('Parabéns')
+    
+
+
+
+
